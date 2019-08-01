ad79c8ef2f2b25286f5a8c4db1781ee6b20c0f05
unknown <morningstar.lux11@gmail.com>
2019-08-01 16:10:02 +0530

    env changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { UserService } from "./core/services/user.service";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'docapp';
+  constructor(private userService: UserService) { }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://localhost:8080/api'
+  apiBaseUrl: 'http://35.154.241.13:8080/api'
 };
 
 /*
